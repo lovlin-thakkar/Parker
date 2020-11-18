5019f0bfdc01b22847bae81dfb12f3021f56c775
Lovlin Thakkar <lovlin@Lovlins-MacBook-Air.local>
2020-11-18 17:22:51 +0530

    fix & add more tests

--- src/test/java/com/squad/parker/CommandProcessorTest.java
+++ src/test/java/com/squad/parker/CommandProcessorTest.java
@@ -27,8 +27,14 @@ public class CommandProcessorTest {
     @Test
     public void processCommandTest() {
         assertEquals("Created parking of 6 slots", commandProcessor.process("Create_parking_lot 6"));
+        assertEquals("Car with vehicle registration number 'KA-01-HH-1234' has been parked at slot number 1", commandProcessor.process("Park KA-01-HH-1234 driver_age 21"));
+        assertEquals("Car with vehicle registration number 'PB-01-HH-1234' has been parked at slot number 2", commandProcessor.process("Park PB-01-HH-1234 driver_age 21"));
+        assertEquals("1, 2", commandProcessor.process("Slot_numbers_for_driver_of_age 21"));
+        assertEquals("Car with vehicle registration number 'PB-01-TG-2341' has been parked at slot number 3", commandProcessor.process("Park PB-01-TG-2341 driver_age 40"));
+        assertEquals("2", commandProcessor.process("Slot_number_for_car_with_number PB-01-HH-1234"));
         assertEquals("Slot number 2 vacated, the car with vehicle registration number 'PB-01-HH-1234' left the space, the driver of the car was of age 21", commandProcessor.process("Leave 2"));
-        assertEquals("Created parking of 6 slots", commandProcessor.process(""));
+        assertEquals("Car with vehicle registration number 'HR-29-TG-3098' has been parked at slot number 2", commandProcessor.process("Park HR-29-TG-3098 driver_age 39"));
+        assertEquals("There are no Cars parked with drivers of age [18].", commandProcessor.process("Vehicle_registration_number_for_driver_of_age 18"));   
     }
     
 }
